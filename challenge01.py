f5a90afda6bb8e5ca9f2a1cbe9f660b9304aadae
Franco Boggiano <137957908+francobg7@users.noreply.github.com>
2024-06-11 22:41:03 -0400

    dia 1
    programa para inversion de una cadena

---
+++ challenge01.py
@@ -0,0 +1,6 @@
+#DIA 1
+#Inversión de una Cadena: Escribe un programa que invierta una cadena de caracteres dada por el usuario.
+
+cadena = input("Por favor, ingrese una cadena de caracteres: ")
+cadena_invertida = cadena[::-1]
+print("La cadena invertida es:", cadena_invertida)
