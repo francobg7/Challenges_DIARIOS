2e62da29a658175ff357ff4a4251909b7a3b1bd3
Franco Boggiano <137957908+francobg7@users.noreply.github.com>
2024-06-20 17:17:05 -0400

    dia 6

---
+++ challenge06.py
@@ -0,0 +1,5 @@
+#DIA 5
+#Conversión de Temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+temp = float(input('ingrese la temperatura: '))
+convertir = temp * 1.8 + 32
+print(f"la temperatura paso de {temp} grados celsius a {convertir} grados fahrenheit")
