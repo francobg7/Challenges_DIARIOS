93a0776a4b1d8c2fa7ff3f6798c9232f1edafa10
Franco Boggiano <137957908+francobg7@users.noreply.github.com>
2024-06-21 19:15:20 -0400

    Primer challenge en c++

---
+++ Challenge9.cpp
@@ -0,0 +1,21 @@
+/*DIA 8
+Escribir un programa que pida al usuario dos números y los sume.¡Pero esta vez hazlo en C++!:)*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int num1, num2,suma;
+
+	cout << "ingrese el primer numero: \n";cin >> num1;
+	cout << "ingrese el segundo numero: \n";cin >> num2;
+
+	suma = num1 + num2;
+	cout << "el resultado de la suma es: " << suma << endl;
+
+
+}
+
